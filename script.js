743f72562ffd02ba2e8bea14ce47df090db20632
Dmitry Mandrin <50268595+2Drotz@users.noreply.github.com>
2022-08-07 16:57:00 +0300

    commit

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const btn = document.querySelectorAll('.btn'),
-   input = document.querySelector('#input');
+   input = document.querySelector('#input'),
+   title = document.querySelector('.title');
 
-
+let phoneAnimation;
 let equals = '';
 
 btn.forEach(element => {
    element.addEventListener('click', () => {
       let str = input.value.slice(-1);
-
+      animate();
       if (element.value == 'C') {
          equals = '';
          input.value = equals;
@@ -24,3 +25,44 @@ btn.forEach(element => {
 
    });
 });
+
+let scale = [
+   {
+      transform: 'translateX(0) translateY(0) rotate(0deg)',
+      filter: 'opacity(100%)',
+   },
+   {
+      transform: 'translateX(20px) translateY(0) rotate(2deg)',
+      filter: 'opacity(50%)'
+   },
+   {
+      transform: 'translateX(20px) translateY(20px) rotate(-2deg)',
+      filter: 'opacity(25%)'
+   },
+   {
+      transform: 'translateX(0) translateY(20px) rotate(2deg)',
+      filter: 'opacity(50%)'
+   },
+   {
+      transform: 'translateX(0) translateY(0) rotate(0deg)',
+      filter: 'opacity(100%)'
+   }
+];
+
+
+let time = {
+   duration: 6000,
+   iterations: Infinity
+};
+
+
+animate = () => {
+   if (!phoneAnimation) {
+      phoneAnimation = title.animate(scale, time);
+
+   } else if (phoneAnimation.playState === 'paused') {
+      phoneAnimation.play();
+   } else {
+      phoneAnimation.pause();
+   }
+};
